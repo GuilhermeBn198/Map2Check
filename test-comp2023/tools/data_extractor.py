10dd0c0f24a4661cf4835534085c4fedfb23fd2d
Guilherme Bernardo <bguilherme51@gmail.com>
2024-01-19 12:57:53 -0400

    Update data_extractor.py

--- test-comp2023/tools/data_extractor.py
+++ test-comp2023/tools/data_extractor.py
@@ -13,12 +13,12 @@ def extrair_informacoes(arquivo_xml, tags):
  data = {}
 
  for tag in tags:
-     # Include the namespace in the tag
-     elementos = root.findall('.//ns:data[@key="{}"]'.format(tag), ns)
-     for elemento in elementos:
-         texto = elemento.text
-         if texto:
-             data[tag] = texto
+    # Include the namespace in the tag
+    elementos = root.findall('.//ns:data[@key="{}"]'.format(tag), ns)
+    for elemento in elementos:
+        texto = elemento.text
+        if texto:
+            data[tag] = texto
 
  return data
 
@@ -26,8 +26,8 @@ def create_metadata_file(data, filename):
  metadata = ET.Element('test-metadata')
 
  for key, value in data.items():
-     if key != 'assumption':
-         ET.SubElement(metadata, key).text = value
+    if key != 'assumption':
+        ET.SubElement(metadata, key).text = value
 
  # Add creationtime tag
  creationtime = datetime.now().isoformat()
@@ -41,16 +41,18 @@ def create_testcase_file(data, filename):
  testcase.set('coversError', 'true')
 
  for value in data:
-     value = value.strip()
-     ET.SubElement(testcase, 'input').text = value.replace('\result == ', '')
+    value = value.strip()
+    if value.startswith('\result == '):
+        value = value[len('\result == '):]
+    ET.SubElement(testcase, 'input').text = value
 
  tree = ET.ElementTree(testcase)
  tree.write(filename, encoding='utf-8', xml_declaration=True)
 
 def create_zip_file(metadata_filename, testcase_filename):
  with zipfile.ZipFile('test-suite.zip', 'w') as zipf:
-     zipf.write(metadata_filename, arcname='metadata.xml')
-     zipf.write(testcase_filename, arcname='testcase.xml')
+    zipf.write(metadata_filename, arcname='metadata.xml')
+    zipf.write(testcase_filename, arcname='testcase.xml')
 
 tags = [
  'sourcecodelang',
