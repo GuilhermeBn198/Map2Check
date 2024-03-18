a449cc34e0ab66bf874fbf1857dc43f9f9180e07
Guilherme Bernardo <bguilherme51@gmail.com>
2024-03-18 13:31:36 -0400

    testsuites do map2check por fazer

--- test-comp2023/tools/testcov-script.bash
+++ test-comp2023/tools/testcov-script.bash
@@ -1,6 +1,7 @@
+sudo install podman
+ls /home/guilherme/github/Map2Check/test-comp2023/tools/testcov
 podman pull registry.gitlab.com/sosy-lab/benchmarking/competition-scripts/user:latest
-podman run --rm -i -t --volume=/mnt/c/Users/Guilherme/Documents/GitHub/Map2Check/test-comp2023/tools:/testcov --workdir=/testcov registry.gitlab.com/sosy-lab/benchmarking/competition-scripts/user:latest bash
+podman run --rm -i -t --volume=/home/guilherme/github/Map2Check/test-comp2023/tools:/testcov --workdir=/testcov registry.gitlab.com/sosy-lab/benchmarking/competition-scripts/user:latest bash
 cd testcov
 pip install tsbuilder
-./bin/testcov --no-isolation --test-suite "../map2check/Test-suitesDoMap
-2check/simple_1-1_abstracted/witness.graphml" "../sv-benchmarks/c/bitvector-regression/implicitfloatconversion.c"
+./bin/testcov --no-isolation --test-suite "test-suite.zip" "programa.c"
