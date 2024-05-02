ca927d3283c7009bf90a4c929821f194f2887155
Guilherme Bernardo <bguilherme51@gmail.com>
2024-05-01 22:34:26 -0400

    :sparkles: propriedades testcomp aceitas pelo wrapper

--- test-comp2023/tools/map2check-script-anotacoes.md
+++ test-comp2023/tools/map2check-script-anotacoes.md
@@ -1,5 +1,21 @@
 # Anotações de modificações no map2check-wrapper.py
 
-- Foi adicionado um elif contendo as propriedades necessárias para concorrer no coverage-branches e coverage-error-calls
-- Foi adicionado duas propriedades que são settadas True, caso a propriedade seja verificada(is_cov_call e is_cov_branches)
-- Foi adicionado um elif que verifica as propriedades True, com base no esbc_wrapper e nas outras propriedades disponíveis, cheguei na linha de comando da linha 89 e 93
+## **DECIDI POR NÃO FAZER UM ARGUMENTO INTEIRO SEPARADO POR MOTIVOS DE: SE AS PROPRIEDADES SÃO UNICAS PERANTE AS DUAS COMPETIÇÕES, NÃO É NECESSÁRIO TER UMA FLAG PARA IDENTICAÇÃO DAS COMPETIÇÕES VISTO QUE ELAS SÃO UNICAS.**
+  
+- Foi adicionado duas propriedades que são settadas True, caso a propriedade seja verificada(is_cov_call e is_cov_branches) (linhas 44 e 45)
+  
+- Foi adicionado dois elif contendo as propriedades necessárias para concorrer no coverage-branches e coverage-error-calls (linhas 62 e 64)
+  
+- Foi adicionado dois elifs que verificam as propriedades True de **is_cov_call e is_cov_branches**, com base no esbc_wrapper e nas outras propriedades disponíveis no map2check, cheguei na linha de comando da linha 89 e 93
+
+  - Observando o comportamento da ferramenta a partir das alterações feitas, pode-se observar que agora, não evidencia erro de propriedade não suportada pela ferramenta, como ocorria antes, dito isto, acredita-se que a abordagem feita tenha obtido relativo sucesso, uma vez que, apesar de não estar detectando erro, a ferramenta ainda não é capaz de reconhecer os arquivos com a função REACH_ERROR
+
+## O comando usado para testar a diferença entre as duas propriedades foi
+
+- **para a versão original:**
+  - python3 map2check-wrapper.py -p ../properties/unreach-call-map2check.prp ../sv-benchmarks/map2checktests/array-industry-pattern/array_range_init.i
+  - python3 map2check-wrapper.py -p ../properties/coverage-error-call.prp ../sv-benchmarks/c/array-industry-pattern/array_range_init.i
+
+- **para a versão modificada:**
+  - python3 map2check-wrapper-modificado.py -p ../properties/unreach-call-map2check.prp ../sv-benchmarks/map2checktests/array-industry-pattern/array_range_init.i
+  - python3 map2check-wrapper-modificado.py -p ../properties/coverage-error-call.prp ../sv-benchmarks/c/array-industry-pattern/array_range_init.i
---
+++ test-comp2023/tools/map2check/map2check-wrapper-modificado.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import shlex
+import subprocess
+import importlib
+
+tool_path = "./map2check "
+# default args
+extra_tool = "timeout 897s "
+command_line = extra_tool + tool_path
+timemapsplit = "895"
+
+# Options
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--version", help="Prints Map2check's version", action='store_true')
+parser.add_argument("-p", "--propertyfile", help="Path to the property file")
+parser.add_argument("benchmark", nargs='?', help="Path to the benchmark")
+
+args = parser.parse_args()
+
+# ARG options
+version = args.version
+property_file = args.propertyfile
+benchmark = args.benchmark
+
+if version == True:
+  os.system(tool_path + "--version")
+  exit(0)
+
+if property_file is None:
+  print("Please, specify a property file")
+  exit(1)
+
+if benchmark is None:
+  print("Please, specify a benchmark to verify")
+  exit(1)
+
+is_memsafety 	= False
+is_reachability = False
+is_overflow 	= False
+is_memcleanup   = False
+is_cov_call = False # testcomp coverage_error_calls category
+is_cov_branches = False # testcomp coverage_branches category
+
+f = open(property_file, 'r')
+property_file_content = f.read()
+
+if "CHECK( init(main()), LTL(G valid-free) )" in property_file_content:
+  is_memsafety = True
+elif "CHECK( init(main()), LTL(G valid-deref) )" in property_file_content:
+  is_memsafety = True
+elif "CHECK( init(main()), LTL(G valid-memtrack) )" in property_file_content:
+  is_memsafety = True
+elif "CHECK( init(main()), LTL(G valid-memcleanup) )" in property_file_content:
+  is_memcleanup = True
+elif "CHECK( init(main()), LTL(G ! overflow) )" in property_file_content:
+  is_overflow = True
+elif "CHECK( init(main()), LTL(G ! call(__VERIFIER_error())) )" in property_file_content:
+  is_reachability = True
+elif "COVER( init(main()), FQL(COVER EDGES(@CALL(reach_error))) )" in  property_file_content:
+  is_cov_call = True # linha adicionada para o Testcomp (coverage-error-calls)
+elif "COVER( init(main()), FQL(COVER EDGES(@DECISIONEDGE)) )" in property_file_content:
+  is_cov_branches = True # linha adicionada para o Testcomp (coverage-branches)
+else:
+  # We don't support termination
+  print("Unsupported Property")
+  exit(1)
+
+
+# Set options
+command_line_bkp = ""
+
+if is_memsafety:
+  command_line += " --timeout "+timemapsplit+" --memtrack --smt-solver yices2 --generate-witness "
+  
+elif is_memcleanup:
+  command_line += " --timeout "+timemapsplit+" --memcleanup-property --smt-solver yices2 --generate-witness "
+  
+elif is_reachability:
+  command_line_bkp = command_line + " --timeout "+timemapsplit+" --smt-solver yices2 --target-function --generate-witness "
+  command_line += " --timeout "+timemapsplit+" --smt-solver yices2 --add-invariants --target-function --generate-witness " 
+  ## propriedade originalmente sendo verificada antes de fazermos as modificações
+  
+elif is_overflow:
+  command_line += " --timeout "+timemapsplit+" --check-overflow --smt-solver yices2 --generate-witness "
+  
+elif is_cov_call:
+  command_line_bkp = command_line + " --timeout "+timemapsplit+" --smt-solver yices2 --target-function --generate-witness "
+  command_line += " --timeout "+timemapsplit+" --smt-solver yices2 --add-invariants --target-function --generate-witness "   
+  ## linha adicionada para o Testcomp (coverage-error-calls)
+  
+elif is_cov_branches:
+  command_line_bkp = command_line + " --timeout "+timemapsplit+"  --check-overflow --smt-solver yices2 --target-function --generate-witness "
+  command_line += " --timeout "+timemapsplit+" --smt-solver yices2 --add-invariants --target-function --generate-witness "   # linha adicionada para o Testcomp (coverage-branches)
+
+print("Verifying with MAP2CHECK ")
+
+# Call MAP2CHECK
+command_line += benchmark
+command_line_bkp += benchmark
+
+print("Command: " + command_line)
+args = shlex.split(command_line)
+p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+(stdout, stderr) = p.communicate()
+print(stdout)
+# print(stderr)
+
+# Check invariant conflict
+if b'failed external call: __map2check_main__' in stderr:
+  if is_reachability:
+    args = shlex.split(command_line_bkp)
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (stdout, stderr) = p.communicate()
+    print(stdout)
+    print(stderr)
+
+
+# Parse output
+if b'Timed out' in stdout:
+  print("Timed out")
+  exit(1)
+
+
+# Error messages
+free_offset     = "\tFALSE-FREE: Operand of free must have zero pointer offset"
+deref_offset    = "\tFALSE-DEREF: Reference to pointer was lost"
+memtrack_offset = "\tFALSE-MEMTRACK"
+memcleanup_offset = "\tFALSE-MEMCLEANUP"
+target_offset   = "\tFALSE: Target Reached"
+overflow_offset = "\tOVERFLOW"
+
+if "VERIFICATION FAILED" in stdout.decode():
+    if free_offset in stdout.decode():
+      print("FALSE_FREE")
+      exit(0)
+
+    if deref_offset in stdout.decode():
+      print("FALSE_DEREF")
+      exit(0)
+
+    if memtrack_offset in stdout.decode():
+      print("FALSE_MEMTRACK")
+      exit(0)
+
+    if memcleanup_offset in stdout.decode():
+      print("FALSE_MEMCLEANUP")
+      exit(0)
+
+    if target_offset in stdout.decode():
+      print("FALSE")
+      importlib.import_module('data_extractor')
+      exit(0)
+
+    if overflow_offset in stdout.decode():
+      print("FALSE_OVERFLOW")
+      exit(0)
+
+
+if "VERIFICATION SUCCEEDED" in stdout.decode():
+  print("TRUE")
+  importlib.import_module('data_extractor')
+  exit(0)
+
+print("UNKNOWN")
+exit(0)
