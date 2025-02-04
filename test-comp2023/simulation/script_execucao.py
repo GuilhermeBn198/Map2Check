3dd0d47823d46b21f2b03c7c5addf7ed693bb642
Guilherme Bernardo <bguilherme51@gmail.com>
2025-02-03 20:23:36 -0400

    chore(script_execucao): compatibilidade com python 3.5.2 adicionada

--- test-comp2023/simulation/script_execucao.py
+++ test-comp2023/simulation/script_execucao.py
@@ -6,87 +6,87 @@ import yaml # type: ignore
 import time
 import glob
 
-# Função para criar pastas para organizar os resultados
+# Funcao para criar pastas para organizar os resultados
 def criar_pasta_destino(base_dir, categoria, subcategoria):
     subcategoria_dir = os.path.join(base_dir, categoria, subcategoria)
     os.makedirs(subcategoria_dir, exist_ok=True)
-    print(f"[INFO] Pasta de destino criada: {subcategoria_dir}")
+    print("[INFO] Pasta de destino criada: {}".format(subcategoria_dir))
     return subcategoria_dir
 
-# Função para ler as subcategorias a partir do arquivo includesfile
+# Funcao para ler as subcategorias a partir do arquivo includesfile
 def ler_includesfile(includesfile_path):
-    print(f"[INFO] Lendo arquivo includesfile: {includesfile_path}")
+    print("[INFO] Lendo arquivo includesfile: {}".format(includesfile_path))
     subcategorias = []
     with open(includesfile_path, "r") as includesfile:
         for linha in includesfile:
             linha = linha.strip()
             if linha and not linha.startswith("#"):  # Ignorar comentários e linhas vazias
                 subcategorias.append(linha)
-    print(f"[INFO] Subcategorias encontradas: {subcategorias}")
+    print("[INFO] Subcategorias encontradas: {}".format(subcategorias))
     return subcategorias
 
-# Função para extrair os arquivos de entrada (input_files) de arquivos .yml
+# Funcao para extrair os arquivos de entrada (input_files) de arquivos .yml
 def extrair_input_files(yml_path, subcategoria):
     input_files = []
-    print(f"[INFO] Processando arquivo .yml: {yml_path}")
+    print("[INFO] Processando arquivo .yml: {}".format(yml_path))
     try:
         with open(yml_path, "r") as file:
             data = yaml.safe_load(file)
             if "input_files" in data:
                 raw_input = data["input_files"].strip("'")
-                input_file_path = f"./sv-benchmarks/c/{subcategoria}/{raw_input}"
+                input_file_path = "./sv-benchmarks/c/{}/{}".format(subcategoria, raw_input)
                 input_files.append(input_file_path)
-                print(f"[INFO] Input file encontrado: {input_file_path}")
+                print("[INFO] Input file encontrado: {}".format(input_file_path))
     except Exception as e:
-        print(f"[ERRO] Erro ao processar {yml_path}: {e}")
+        print("[ERRO] Erro ao processar {}: {}".format(yml_path, e))
     return input_files
 
-# Função para processar os arquivos dentro de uma subcategoria
+# Funcao para processar os arquivos dentro de uma subcategoria
 def processar_subcategoria(subcategoria_path, destino, categoria, subcategoria_nome):
     subcategoria_path = os.path.join("sv-benchmarks/c/", subcategoria_path)
-    print(f"[INFO] Processando subcategoria: {subcategoria_nome} em {subcategoria_path}")
+    print("[INFO] Processando subcategoria: {} em {}".format(subcategoria_nome,subcategoria_path))
 
     # Debug do caminho
-    print(f"[DEBUG] Caminho final para glob: {subcategoria_path}")
+    print("[DEBUG] Caminho final para glob: {}".format(subcategoria_path))
 
     # Coletar arquivos YML
     arquivos_yml = glob.glob(subcategoria_path)
-    # print(f"[DEBUG] Arquivos encontrados: {arquivos_yml}")
+    # print("[DEBUG] Arquivos encontrados: {}".format(arquivos_yml))
     tempos = []
 
     if not arquivos_yml:
-        print(f"[ERRO] Nenhum arquivo .yml encontrado em {subcategoria_path}")
+        print("[ERRO] Nenhum arquivo .yml encontrado em {}".format(subcategoria_path))
         return
 
     for yml_file in arquivos_yml:
         input_files = extrair_input_files(yml_file, subcategoria_nome)
         for input_file in input_files:
-            comando = ["python3", "Map2Check/release/map2check-wrapper.py", "-p", "coverage-error-call.prp", input_file]
-            print(f"[INFO] Executando ferramenta para o arquivo: {input_file}")
+            comando = ["python3", "release/map2check-wrapper.py", "-p", "coverage-error-call.prp", input_file]
+            print("[INFO] Executando ferramenta para o arquivo: {}".format(input_file))
             tempo_execucao, test_suite_path = executar_ferramenta(comando, input_file, destino)
-            time.sleep(1)  # Delay para visualização clara no terminal
+            time.sleep(1)  # Delay para visualizacao clara no terminal
 
             if tempo_execucao is not None and test_suite_path:
-                print(f"[INFO] Test-suite.zip gerado: {test_suite_path}")
+                print("[INFO] Test-suite.zip gerado: {}".format(test_suite_path))
                 resultado_testcov, output_file = executar_testcov(test_suite_path, input_file, destino)
                 tempos.append((os.path.basename(input_file), tempo_execucao, resultado_testcov, output_file))
                 print("")
                 time.sleep(1)
             else:
-                print(f"[ERRO] Falha ao gerar test-suite.zip para {input_file}")
+                print("[ERRO] Falha ao gerar test-suite.zip para {}".format(input_file))
                 tempos.append((os.path.basename(input_file), tempo_execucao, "Erro", None))
                 print("")
 
     tempos_file = os.path.join(destino, "tempos.txt")
     with open(tempos_file, "w") as f:
         for arquivo, tempo, resultado, output_file in tempos:
-            # Reduzir a precisão do tempo para 3 casas decimais
-            tempo_formatado = f"{tempo:.3f}" if tempo is not None else "N/A"
-            linha = f"{arquivo}: {tempo_formatado} segundos, TestCov: {resultado}"
+            # Reduzir a precisao do tempo para 3 casas decimais
+            tempo_formatado = "{:.3f}".format(tempo) if tempo is not None else "N/A"
+            linha = "{}: {} segundos, TestCov: {}".format(arquivo, tempo_formatado, resultado)
             if output_file:
-                linha += f", Saída: {output_file}"
+                linha += ", Saída: {}".format(output_file)
             f.write(linha + "\n")
-    print(f"[INFO] Arquivo tempos.txt gerado: {tempos_file}")
+    print("[INFO] Arquivo tempos.txt gerado: {}".format(tempos_file))
     print("")
     print("")
     print("")
@@ -94,16 +94,16 @@ def processar_subcategoria(subcategoria_path, destino, categoria, subcategoria_n
     print("")
 
 
-# Função principal para processar categorias e subcategorias
+# Funcao principal para processar categorias e subcategorias
 def processar_tarefas(map2check_file, resultados_dir):
-    print(f"[INFO] Processando tarefas a partir de {map2check_file}")
+    print("[INFO] Processando tarefas a partir de {}".format(map2check_file))
     tree = ET.parse(map2check_file)
     root = tree.getroot()
 
     for tasks in root.findall("tasks"):
         categoria = tasks.get("name")
         includesfile_path = tasks.find("includesfile").text
-        print(f"[INFO] Processando categoria: {categoria}")
+        print("[INFO] Processando categoria: {}".format(categoria))
 
         if includesfile_path and os.path.exists(includesfile_path):
             subcategorias = ler_includesfile(includesfile_path)
@@ -115,9 +115,9 @@ def processar_tarefas(map2check_file, resultados_dir):
                 print("")
                 print("")    
         else:
-            print(f"[ERRO] Arquivo includesfile não encontrado: {includesfile_path}")
+            print("[ERRO] Arquivo includesfile nao encontrado: {}".format(includesfile_path))
 
-# Função para executar a ferramenta principal e medir o tempo de execução
+# Funcao para executar a ferramenta principal e medir o tempo de execucao
 def executar_ferramenta(comando, arquivo, output_dir):
     try:
         inicio = time.time()
@@ -133,10 +133,10 @@ def executar_ferramenta(comando, arquivo, output_dir):
         else:
             return tempo_execucao, None
     except subprocess.CalledProcessError as e:
-        print(f"[ERRO] Erro ao executar a ferramenta para {arquivo}: {e}")
+        print("[ERRO] Erro ao executar a ferramenta para {}: {}".format(arquivo,e))
         return None, None
 
-# Função para analisar o arquivo test-suite.zip usando o testcov
+# Funcao para analisar o arquivo test-suite.zip usando o testcov
 def executar_testcov(test_suite_path, arquivo_original, output_dir):
     try:
         comando = [
@@ -150,7 +150,7 @@ def executar_testcov(test_suite_path, arquivo_original, output_dir):
             subprocess.run(comando, check=True, stdout=output, stderr=output)
         return "Sucesso", output_file
     except subprocess.CalledProcessError as e:
-        print(f"[ERRO] Erro ao executar testcov para {test_suite_path}: {e}")
+        print("[ERRO] Erro ao executar testcov para {}: {}".format(test_suite_path,e))
         return "Erro", None
 
 if __name__ == "__main__":
@@ -159,7 +159,7 @@ if __name__ == "__main__":
     os.makedirs(resultados_dir, exist_ok=True)
     # Obter e exibir o caminho completo
     caminho_completo = os.path.abspath(resultados_dir)
-    print(f"Pasta criada/em uso: {caminho_completo}")
-    input("aguardando confirmação visual do operador(press enter)...")
+    print("Pasta criada/em uso: {}".format(caminho_completo))
+    input("aguardando confirmacao visual do operador(press enter)...")
     
     processar_tarefas(map2check_file, resultados_dir)
