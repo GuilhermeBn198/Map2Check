ee70d1a00a311214b56271a1ab08cdf5865f45fa
Guilherme Bernardo <bguilherme51@gmail.com>
2025-02-26 10:09:11 -0400

    chore(results): att graficos totais

--- test-comp2023/simulation/gerar-graficos/results-totais.py
+++ test-comp2023/simulation/gerar-graficos/results-totais.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
 # Dados das torres
-categorias = ['total_FALSE', 'total_TRUE', 'total_coverage_pos','total_coverage_0.0', 'total_UNKNOWN']
-valores = [288, 702, 265, 724, 661]
+categorias = ['FALSE', 'TRUE', 'UNKNOWN', 'COV=0.0', 'COV>0.0']
+valores = [288,	 702,	661,	724,	265]
 
 # Definir cores para as torres
-cores = ['blue', 'blue', 'green', 'red', 'grey']
+cores = ['blue', 'blue', 'orange', 'grey', 'grey']
 
 # Altura máxima do gráfico (valor de total_program_files)
 altura_max = 1651
@@ -14,22 +14,27 @@ altura_max = 1651
 plt.figure(figsize=(10, 6))
 barras = plt.bar(categorias, valores, color=cores)
 
-# Definir título e rótulos dos eixos
-plt.title('Programas executados',fontsize=18)
-plt.xlabel('Resultados',fontsize=16)
-plt.ylabel('Total de programas',fontsize=16)
+# Definir título e rótulos dos eixos com fontes ajustadas
+plt.title('Programas executados', fontsize=18)
+plt.xlabel('Resultados', fontsize=16)
+plt.ylabel('Total de programas', fontsize=16)
 
-# Ajustar tamanho dos rótulos do eixo x e girá-los para melhor visualização
+# Ajustar tamanho dos rótulos do eixo x (com rotação de 10 graus) e eixo y
 plt.xticks(fontsize=14, rotation=10)
 plt.yticks(fontsize=14)
 
-# Definir a altura máxima do gráfico
+# Definir a altura máxima do gráfico (eixo y)
 plt.ylim(0, altura_max)
 
 # Exibir os valores acima de cada barra
 for barra in barras:
     y_valor = barra.get_height()
-    plt.text(barra.get_x() + barra.get_width()/2, y_valor + 10, str(y_valor), ha='center', va='bottom', fontsize=14)
+    plt.text(barra.get_x() + barra.get_width()/2, y_valor + 10, 
+             str(y_valor), ha='center', va='bottom', fontsize=14)
+
+# Adicionar anotação com o valor máximo do eixo y (total_program_files)
+plt.text(0.95, 0.95, f'Max: {altura_max}', transform=plt.gca().transAxes,
+         ha='right', va='top', fontsize=16, fontweight='bold', color='green')
 
 # Exibir o gráfico
 plt.show()
